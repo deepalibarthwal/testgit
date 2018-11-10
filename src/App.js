89e7df740df1a5a735465e31f55429f72bb88a41
Deepali Barthwal <deepali.barthwal@algorisys.com>
2018-11-10 10:37:00 +0530

    Test5

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
-          <p>Test4</p>
+          <p>Test5</p>
           <a
             className="App-link"
             href="https://reactjs.org"
